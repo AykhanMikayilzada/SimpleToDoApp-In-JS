e706fd76bd872526ca6d6afd80acaa1beb379965
Aykhan Mikayilzada <130388657+AykhanMikayilzada@users.noreply.github.com>
2023-11-17 00:43:43 +0400

    Simple ToDoApp-JS
    I create simple ToDoApp in JavaScript

---
+++ assets/Js/toDo.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const info = document.querySelector(".info");
+const list = document.querySelector(".list")
+let data = [];
+button.addEventListener("click", function () {
+  let value = input.value;
+  data.push(value);
+  
+  let result = data.map(function (el,index) {
+    return `<li>${index+1+")"} ${el}</li>`
+  }).join("")
+
+  list.innerHTML = result;
+  input.value = ""
+  
+});
