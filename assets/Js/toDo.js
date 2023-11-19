da77395aadfb31c4e15ccb3fcb5e3007f04688c5
Aykhan Mikayilzada <130388657+AykhanMikayilzada@users.noreply.github.com>
2023-11-19 16:22:22 +0400

    Update toDo.js

--- assets/Js/toDo.js
+++ assets/Js/toDo.js
@@ -1,21 +1,41 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const info = document.querySelector(".info");
-const list = document.querySelector(".list")
+const list = document.querySelector(".list");
 let data = [];
 button.addEventListener("click", function () {
   let value = input.value;
-  
-  if(value === '') {
-    alert("Zəhmət olmasa məlumatı daxil edin.")
-  }
-  else {
+
+  if (value === "") {
+    alert("Zəhmət olmasa məlumatı daxil edin.");
+  } else {
     data.push(value);
   }
-  let result = data.map(function (el,index) {
-    return `<li>${index+1+")"} ${el}</li>`
-  }).join("")
+  let result = data
+    .map(function (el, index) {
+      return `<li>${index + 1 + ")"} ${el}</li>`;
+    })
+    .join("");
 
   list.innerHTML = result;
-  input.value = ""
+  input.value = "";
+});
+window.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    let value = input.value;
+
+    if (value === "") {
+      alert("Zəhmət olmasa məlumatı daxil edin.");
+    } else {
+      data.push(value);
+    }
+    let result = data
+      .map(function (el, index) {
+        return `<li>${index + 1 + ")"} ${el}</li>`;
+      })
+      .join("");
+
+    list.innerHTML = result;
+    input.value = "";
+  }
 });
